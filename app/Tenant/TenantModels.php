acd38e28417e223b483c07b6cf55dc8c5016517a
Deigon Prates <deigon_prates@hotmail.com>
2021-02-02 20:32:33 -0300

    Evento creating para inserir tenant automaticamente

--- app/Tenant/TenantModels.php
+++ app/Tenant/TenantModels.php
@@ -2,13 +2,20 @@
 
 namespace App\Tenant;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait TenantModels
 {
-   
+
     protected static function boot()
     {
         parent::boot();
 
         static::addGlobalScope(new TenantScope());
+
+        static::creating(function (Model $obj){
+            $company_id = \Auth::user()->company_id;
+            $obj->company_id = $company_id;
+        });
     }
 }
